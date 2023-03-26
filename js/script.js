ba1ccc55f6e3912a3fc47252ba5304226cb95539
afaysalk <91470600+afaysalk@users.noreply.github.com>
2023-03-26 04:33:08 +0300

    Setting up wallpaper functionality

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // Make the DIV element draggable:
 dragElement(document.getElementById("drag"));
 
+
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
   if (document.getElementById(elmnt.id + "header")) {
