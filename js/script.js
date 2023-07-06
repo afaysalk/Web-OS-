44f3f8a4c9043b39bec1c57c4a7c0a5acbb7990c
afaysalk <afaysalkhaldi@gmail.com>
2023-07-06 18:44:05 +0100

    Added hello svg animation to about page

--- js/script.js
+++ js/script.js
@@ -21,9 +21,9 @@ dragElement(document.getElementById("error"));
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
+  if (document.getElementById(elmnt.id + "_header")) {
     // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    document.getElementById(elmnt.id + "_header").onmousedown = dragMouseDown;
   } 
 
   function dragMouseDown(e) {
